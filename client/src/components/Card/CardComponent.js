c2a6578ecf04d8f15eb93e47aad0239825deb258
Varbanov <stavar01@protiviti.com>
2021-12-08 23:42:17 +0200

    details button added

--- client/src/components/Card/CardComponent.js
+++ client/src/components/Card/CardComponent.js
@@ -1,19 +1,21 @@
 
+import { useReducer } from 'react';
 import './Card.css'
 const CardComponent = ({
     note
 }) => {
 
+    
     return (
-
-
         <div className="card">
-
             <div id='container'><h4 id="card-title">{note.title}</h4>
                 <button className="card-button state">Move state</button>
-                <button className="card-button-delete" >Delete</button> </div>
+                <button className="card-button-delete" >Delete</button>
+                <button className="card-button-details">...</button>
 
-        </div>
+            </div>
+
+        </div >
 
 
     );
